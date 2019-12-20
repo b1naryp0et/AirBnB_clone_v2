be9392964272004473427012efa2f34cf2f807b2
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-12-20 17:06:53 -0500

    add delete method, update all to filter for class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,19 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        if cls:
+            return {
+                key: self.__objects[key]
+                for key in self.__objects
+                if self.__objects[key].__class__ == cls
+            }
+        else:
+            return self.__objects
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +62,9 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Delete obj from _objects """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
