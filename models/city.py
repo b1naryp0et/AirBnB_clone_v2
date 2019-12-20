71cd331b8a9b0390531cdad83da532f99ef97857
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-12-20 18:02:44 -0500

    adding db_storage

--- models/city.py
+++ models/city.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """This is the city class"""
 from models.base_model import BaseModel
+from models.state import State
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey(State.id), nullable=False)
+    # foreign key might need more work
--- models/state.py
+++ models/state.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
-    Attributes:
-        name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    """TODO:
+    for DBStorage: class attribute cities must represent a relationship with
+    the class City. If the State object is deleted, all linked City objects
+    must be automatically deleted. Also, the reference from a City object to
+    his State should be named state
+    for FileStorage: getter attribute cities that returns the list of City
+    instances with state_id equals to the current State.id => It will be the
+    FileStorage relationship between State and City
+    """
