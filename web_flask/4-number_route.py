7c643d564cdede4a9270d1cf8e9fa96ca49682ca
Faizan <mathemagician229@gmail.com>
2020-01-22 17:10:12 -0500

    Adding completed tasks 2, 3, and 4.

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Script which starts a Flask web application """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def start_flask():
+    """ Route returning the specified string """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ Route returning the specified string """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_deez(text):
+    """ Displays C, then text variable """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={"text": "is cool"})
+@app.route('/python/<text>')
+def py_text(text):
+    """ Returns python, then text string """
+    return 'Python %s' % escape(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def check_num(n):
+    """ Displays text upon int """
+    return '%d is a number' % n
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
